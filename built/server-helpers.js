734ea110b36e53e9fd2163980410d7cfec19ee81
hktari <bkamnik1995@gmail.com>
2022-07-29 15:54:34 +0200

    ignore built folder

--- built/server-helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addHeadersToResponse = void 0;
-function addHeadersToResponse(res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-}
-exports.addHeadersToResponse = addHeadersToResponse;
-//# sourceMappingURL=server-helpers.js.map
