4cc5bd951a7540ba144d11b14a0181101178128f
hktari <bkamnik1995@gmail.com>
2022-07-29 17:57:53 +0200

    add to local user controller

--- src/local-user.ts
+++ src/local-user.ts
@@ -27,17 +27,40 @@ export async function createOne(req: Request, res: Response, next: NextFunction)
     }
 }
 
-export async function getAll(req:Request, res:Response, next:NextFunction) {
+export async function getAll(req: Request, res: Response, next: NextFunction) {
     console.log('getAll: [GET] /users/');
     try {
         const ALL = await User.findAll()
         console.log(
             'OK getAll USER: ',
-            ALL.map((el:any) => el.dataValues),
+            ALL.map((el: any) => el.dataValues),
         )
         return res.status(200).json(ALL);
     } catch (error) {
         console.error('getAll USER', error);
         return res.status(500).json(error);
     }
+}
+
+export async function getOne(req: Request, res: Response, next: NextFunction) {
+    const ID = req.params.id;
+    console.log('getOne: [GET] /users/' + ID)
+
+    try {
+        const USER = await User.findByPk(ID)
+        console.log('OK getOne USER: ', USER.dataValues)
+        return res.status(200).json(USER);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error);
+    }
+}
+
+
+export async function updateOne(req: Request, res: Response, next: NextFunction) {
+    return res.status(400).write("not implemented");
+}
+
+export async function deleteOne(req: Request, res: Response, next: NextFunction) {
+    return res.status(400).write('not implemented');
 }
