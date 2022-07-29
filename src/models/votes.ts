ca1556589385f332c8abae38d5b74ab0722de4a7
hktari <bkamnik1995@gmail.com>
2022-07-29 19:15:07 +0200

    add votes model

---
+++ src/models/votes.ts
@@ -0,0 +1,31 @@
+import { emptyQuery } from 'pg-protocol/dist/messages'
+import * as Sequelize from 'sequelize'
+import db from '../util/database'
+
+const Votes = db.define('votes', {
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    userId:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+            model: db.models.users,
+            key: 'id'
+        }   
+    },
+    quoteId:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+            model: db.models.quotes,
+            key: 'id'
+        }   
+    },
+    timestamp:{
+        type: Sequelize.DATE
+    }
+});
