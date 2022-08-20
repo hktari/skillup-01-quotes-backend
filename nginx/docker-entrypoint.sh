15454718d4a94a9691971e3384d7e11436b9f342
hktari <bkamnik1995@gmail.com>
2022-08-20 12:28:30 +0200

    add DOMAIN as config var

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -eu
 
 echo 'ENVSUBST'
 
-envsubst '${API_HOST} ${API_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${API_HOST} ${API_PORT} ${DOMAIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 cat /etc/nginx/conf.d/default.conf
 
